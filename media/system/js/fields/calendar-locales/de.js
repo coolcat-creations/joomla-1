a6a0b108280452a001dc66bb7943bcd0a1193f70
coolcat-creations <mail@coolcat-creations.com>
2017-03-06 09:35:35 +0100

    Update de.js
    Shorten the days to 2 letters , i think that´s more common

--- media/system/js/fields/calendar-locales/de.js
+++ media/system/js/fields/calendar-locales/de.js
@@ -4,7 +4,7 @@ window.JoomlaCalLocale = {
 	wk : "KW",
 	time : "Uhrzeit:",
 	days : ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
-	shortDays : ["Son", "Mon", "Die", "Mit", "Don", "Fre", "Sam"],
+	shortDays : ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
 	months : ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
 	shortMonths : ["Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"],
 	AM : "AM",
